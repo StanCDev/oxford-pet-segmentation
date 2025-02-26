666ddafb0a01e3a1b06fe9d12926843bc67d536d
RTGT2021 <85016854+RTGT2021@users.noreply.github.com>
2025-02-26 17:48:07 +0000

    Code for augmentation and testing augmentation
    Added code to augmentation.py in src directory and created a test_augmentation.py in the test directory.
    
    Transformations currently included are color jitter, random rotation and horizontal flip.
    
    Created a new test\res_aug to save the transformations
    
    included a label directory in the test\res, the test\res_out and the test\res_aug to save replica transformations on the masks
    
    Still require to work on:
    - add additional transformation and decide on probability factor and any limits
    - use to augment the dataset ( at the moment just saves one transformed image)
    - look at masks output from transformation to ensure we preserve the 0, 1, 2 and 255 labels

---
+++ src/preprocessing/augmentation.py
@@ -0,0 +1,86 @@
+"""
+This module provides functions to augment images in a common directory using multiple augmentation options
+"""
+from pathlib import Path
+from torchvision.transforms import v2
+from PIL import Image
+import numpy as np
+
+def augment_image(
+    img : np.ndarray,
+    img_lab : np.ndarray,
+    col_jit : float, 
+    rand_rot : float, 
+    rand_hflip : float 
+    ) -> np.ndarray: 
+    """
+    augment a numpy array of type uint8 with color jitter, random rotation and random horizontal flip
+
+    Args:
+    -----
+        img : image to augment
+        img_lab : label image to augment
+
+        col_jit (float): color jitter probability factor
+        rand_rot (float): random rotation probability factor
+        rand_hflip (float): random horizontal flip probability factor
+
+    Returns:
+    --------
+        (np.ndarrray) augmented image and (np.ndarray) label image
+    """
+    transform = v2.Compose([
+        v2.ColorJitter(brightness=col_jit, contrast=col_jit, saturation=col_jit, hue=col_jit),
+        v2.RandomRotation(degrees=rand_rot),
+        v2.RandomHorizontalFlip(p=rand_hflip)])
+    return transform(img, img_lab) 
+
+def aug_directory(
+        src_dir : Path, 
+        dest_dir: Path, 
+        src_dir_lab : Path,  
+        dest_dir_lab: Path,  
+        col_jit : float,
+        rand_rot : float,
+        rand_hflip : float,
+        print_progress : bool = True
+        ) -> None:
+    """
+    Augments all images in a directory with defined probablity factors and apply the same augmentation to label images with the same name
+
+    Args:
+    -----
+        src_dir (Pathlib.Path): path where source images are located.
+        dest_dir (Pathlib.Path): path where resized images are saved.
+        src_dir_lab (Pathlib.Path): path where source label images are located.
+        dest_dir_lab (Pathlib.Path): path where resized label images are saved.
+
+        col_jit (float): color jitter factor
+        rand_rot (float): random rotation factor
+        rand_hflip (float): random horizontal flip factor
+
+        print_progress (bool): obtain a progress bar showing number of resized images out of total count, default=True.
+
+    Returns:
+    --------
+        None
+    """
+    accepted_file_types = {".jpg", ".jpeg", ".png"}
+    count = 0
+    dir_files = src_dir.iterdir()
+    dir_files_lab = src_dir_lab.iterdir()  
+    nbr_images = len(list(filter(lambda image_path: (image_path.suffix.lower() in accepted_file_types) , dir_files)))
+    nbr_labels = len(list(filter(lambda image_path: (image_path.suffix.lower() in accepted_file_types) , dir_files_lab))) 
+
+    for image_path in src_dir.iterdir():
+        if image_path.suffix.lower() in accepted_file_types:
+             with Image.open(image_path) as img, Image.open(src_dir_lab / f"{Path(image_path).stem}.png") as img_lab: # RT added to open label image (***hard wired .png extension at the moment***)
+                img = img.convert("RGB")  
+                img_lab = img_lab.convert("L") # Open in grayscale mode to limit impact of color jitter on masks (need to investigate as may be better to have a separate colorjitter function for the images only)
+                count += 1
+                out_image, out_lab = augment_image(img=img, img_lab = img_lab, col_jit=col_jit, rand_rot=rand_rot, rand_hflip=rand_hflip)   
+                out_image.save(dest_dir / f"{Path(image_path).name}")
+                out_lab.save(dest_dir_lab / f"{Path(image_path).stem}.png") 
+                if print_progress:
+                    print(f"Augmented {count} images / {nbr_images}")
+    return
---
+++ test/test_augmentation.py
@@ -0,0 +1,19 @@
+import pytest
+import sys
+from pathlib import Path
+
+# Add the src directory to sys.path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent / "src"))
+
+from preprocessing.augmentation import aug_directory, augment_image
+
+
+def test_aug_directory():
+    src_dir = Path("res_out/")
+    dest_dir = Path("res_aug/")
+# dest_dir_lab and src_dir_lab added for labels
+    src_dir_lab = Path("res_out/label")
+    dest_dir_lab = Path("res_aug/label")
+# dest_dir_lab and src_dir_lab included for labels
+    aug_directory(src_dir, dest_dir, src_dir_lab, dest_dir_lab, col_jit = 0, rand_rot = 0.5, rand_hflip = 0.5) # Test with % probability for colorjitter, % for random rotation and % for random horizontal flip
+    return
